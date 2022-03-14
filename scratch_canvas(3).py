d349c45802d0e937c618659f8f97935e4513997d
Deven khichar <95070492+devenkhichar@users.noreply.github.com>
2022-03-14 03:32:52 -0700

    Add files via upload

---
+++ scratch_canvas(3).py
@@ -0,0 +1,16 @@
+n1 = int(input("Type the first side of the calculation : "))#Take the user input for the first number and while taking user input print the statement ("Enter the first number")
+n2 = int(input("Type the second side of the calculation"))
+
+def addition(n1,n2):
+    print(n1+n2)
+def subtraction(n1,n2):
+    print(n1-n2)
+def multliply(n1,n2):
+    print(n1*n2)
+def divide(n1,n2):#Create functions for multiplication and division
+    print(n1/n2)
+addition(n1,n2)
+subtraction(n1,n2)
+multliply(n1,n2)
+divide(n1,n2)
+#Call both the functions and pass parameters n1, n2
